555bceedd2103c6697adfcd09fda7a5b1e23500b
HardreaM <zubkovandrej6@gmail.com>
2024-02-29 00:55:45 +0500

    Reformatting

--- CarApi/Dal/Cars/Models/CarOwnerDal.cs
+++ CarApi/Dal/Cars/Models/CarOwnerDal.cs
@@ -6,15 +6,20 @@ namespace Dal.Cars.Models;
 [Table("car_owners")]
 public record CarOwnerDal : BaseEntityDal<Guid>
 {
-    [Column("name")] public required string Name { get; init; }
+    [Column("name")]
+    public required string Name { get; init; }
 
-    [Column("surname")] public required string Surname { get; init; }
+    [Column("surname")]
+    public required string Surname { get; init; }
 
-    [Column("login")] public required string Login { get; init; }
+    [Column("login")]
+    public required string Login { get; init; }
 
-    [Column("email")] public required string Email { get; init; }
+    [Column("email")]
+    public required string Email { get; init; }
 
-    [Column("phone")] public required string Phone { get; init; }
+    [Column("phone")]
+    public required string Phone { get; init; }
 
     public List<CarDal> Cars { get; set; }
 }
